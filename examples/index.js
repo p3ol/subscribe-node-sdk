a5c0629c4a3d367e8c97db1f373211be9a4a7e53
Ugo Stephant <ugo@poool.fr>
2022-04-13 07:59:58 +0200

    feat: add auth methods

--- examples/index.js
+++ examples/index.js
@@ -1,13 +1,28 @@
+/* eslint-disable no-console */
 const Subscribe = require('../dist/index.cjs.js');
 
 const subs = Subscribe({
-  clientId: '0893042862168465',
-  clientSecret: '23WfqB4TbdKm1TnkQ0QrAIv4nZuQcHQ9',
+  clientId: '0893542862168465',
+  clientSecret: '23Wf5B4TbdKm1TnkQ0QrAIv4nZuQcHQ9',
+  debug: false,
 });
 
 (async () => {
   try {
-    console.log(await subs.offers.list({ exclude: ['premium'] }));
+    // Offers
+    const offers = await subs.offers.list({ exclude: ['premium'] });
+    console.log('offers.list', offers);
+
+    // Auth
+    const callback = await subs.auth.createCallback({
+      username: 'test@poool.fr',
+      password: 'test',
+      redirectUri: 'http://localhost',
+    });
+    console.log('auth.createCallback', callback);
+
+    const verify = await subs.auth.verifyCallback(callback.callbackId);
+    console.log('auth.verifyCallback', verify);
   } catch (e) {
     console.error(e);
   }
