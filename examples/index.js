2566c2fa742c18bdecf3fed24eca2782b97aa81e
dackmin <ugo@poool.fr>
2022-04-13 09:09:36 +0200

    feat: enable auth for all requests

--- examples/index.js
+++ examples/index.js
@@ -2,8 +2,8 @@
 const Subscribe = require('../dist/index.cjs.js');
 
 const subs = Subscribe({
-  clientId: '0893542862168465',
-  clientSecret: '23Wf5B4TbdKm1TnkQ0QrAIv4nZuQcHQ9',
+  clientId: '0893042862168465',
+  clientSecret: '23WfqB4TbdKm1TnkQ0QrAIv4nZuQcHQ9',
   debug: false,
 });
 
@@ -14,14 +14,20 @@ const subs = Subscribe({
     console.log('offers.list', offers);
 
     // Auth
-    const callback = await subs.auth.createCallback({
-      username: 'test@poool.fr',
-      password: 'test',
-      redirectUri: 'http://localhost',
+    const tokens = await subs.auth.signin({
+      username: 'ugo+test1@poool.fr',
+      password: 'lol',
     });
+    console.log('auth.signin', tokens);
+
+    const callback = await subs.auth.createCallback({
+      grantType: 'bearer',
+      redirectUri: 'http://localhost',
+    }, { auth: tokens });
     console.log('auth.createCallback', callback);
 
-    const verify = await subs.auth.verifyCallback(callback.callbackId);
+    const verify = await subs.auth
+      .verifyCallback(callback.callbackId, { auth: tokens });
     console.log('auth.verifyCallback', verify);
   } catch (e) {
     console.error(e);
